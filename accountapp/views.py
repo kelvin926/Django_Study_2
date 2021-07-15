dcd689d699b87a5f31b11e905ad26b0d44d35cd7
kelvin926 <aa645900@naver.com>
2021-07-15 22:46:26 +0900

    bug fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,18 +13,23 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get("hello_world_input")
-        
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+    
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get("hello_world_input")
+            
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+            
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', 
+                            context={'hello_world_list': hello_world_list})
 
+    else:
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
