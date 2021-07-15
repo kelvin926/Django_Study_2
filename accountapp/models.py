992d37838baf32428067cefbc7470ee140b72f80
kelvin926 <aa645900@naver.com>
2021-07-15 22:26:54 +0900

    django 26 - Bug Fix

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class HelloWorld(models.Model):
-    text = models.CharField(max_length=255, null=False) # null : ¾ø¾îµµ µÇ´ÂÁö.
+    text = models.CharField(max_length=255, null=False) # null : ì—†ì–´ë„ ë˜ëŠ”ì§€.
--- kelvin/urls.py
+++ kelvin/urls.py
@@ -19,7 +19,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accountapp.urls')),
+    path('accounts/', include('accountapp.urls')),
 ]
 
 
